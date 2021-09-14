4cd040fc77a9ac947aadf97151411ba12fb139ab
Laura White <law7676@outlook.com>
2021-09-14 10:58:07 -0500

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,14 @@ const friends = [
   }
 ];
 
+app.use((req, res, next) => {
+  const start = Date.now();
+  next();
+  const delta = Date.now() - start;
+  console.log(`${req.method} ${req.url} ${delta}ms`);
+  console.log(`Your request took ${delta} miliseconds.`);
+});
+
 app.get('/', (req, res) => {
   res.send('Hello, welcome to my app');
 })
