e0f360f1f5c2fc19904f9efca116e3e1b1cb31fe
Laura <law7676@outlook.com>
2021-09-15 07:49:08 -0500

    modified server.js and added controllers

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
+const friendsController = require('./controllers/friends.controllers');
+const messagesController = require('./controllers/messages.controllers');
+
 const app = express();
 
 const PORT = 3000;
@@ -23,33 +26,21 @@ app.use((req, res, next) => {
   console.log(`Your request took ${delta} miliseconds.`);
 });
 
+app.use(function (err, req, res, next) {
+  console.log(err);
+  res.status(400).send('Something is not working');
+})
+
 app.get('/', (req, res) => {
   res.send('Hello, welcome to my app');
 })
 
-app.get('/friends', (req, res) => {
-  res.json(friends);
-});
+app.post('/friends', friendsController.postFriend);
+app.get('/friends', friendsController.getFriends);
+app.get('/friends/:friendId', friendsController.getFriend);
 
-app.get('/friends/:friendId', (req, res) => {
-  const friendId = Number(req.params.friendId);
-  const friend = friends[friendId];
-  if (friend) {
-    res.status(200).json(friend);
-  } else {
-    res.status(404).json({
-      error: "Friend does not exist",
-    });
-  }
-})
-
-app.get('/messages', (req, res) => {
-  res.send('<ul><li>Hello Albert!</li></ul>');
-});
-
-app.post('/messages', (req, res) => {
-  console.log('Updating messages...');
-});
+app.get('/messages', messagesController.getMessages);
+app.post('/messages', messagesController.postMessage);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}...`);
