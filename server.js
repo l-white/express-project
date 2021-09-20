5f3dcd679f7608609a3fe4e0926c619d689466e7
White <whitel1@milwaukee.k12.wi.us>
2021-09-20 14:47:14 -0500

    modified controlers, server, and json files; added routers

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
-const friendsController = require('./controllers/friends.controllers');
-const messagesController = require('./controllers/messages.controllers');
+const messagesRouter = require('./routers/messages.router.js');
+const friendsRouter = require('./routers/friends.router.js');
 
 const app = express();
 
@@ -13,24 +13,16 @@ app.use((req, res, next) => {
   const start = Date.now();
   next();
   const delta = Date.now() - start;
-  console.log(`${req.method} ${req.url} ${delta}ms`);
+  console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
   console.log(`Your request took ${delta} miliseconds.`);
 });
 
 app.get('/', (req, res) => {
   res.send('Hello, welcome to my app');
-})
-
-const friendsRouter = express.Router();
-
-friendsRouter.post('/', friendsController.postFriend);
-friendsRouter.get('/', friendsController.getFriends);
-friendsRouter.get('/:friendId', friendsController.getFriend);
+});
 
 app.use('/friends', friendsRouter);
-
-app.get('/messages', messagesController.getMessages);
-app.post('/messages', messagesController.postMessage);
+app.use('/messages', messagesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}...`);
